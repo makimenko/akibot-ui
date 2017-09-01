cb081cf5304572f450aceb0c6dfbc64434bfe1b7
Mihail Akimenko <mihails.akimenko@gmail.com>
2017-09-01 17:53:35 +0300

    material2 design

--- src/APP/app.module.ts
+++ src/APP/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { SceneComponent } from "./scene/scene.component";
 import { WebSocketService } from "./scene/serices/web-socket.service";
+import { Material2Module } from "./material2.module";
+
 
 
 @NgModule({
@@ -16,7 +19,9 @@ import { WebSocketService } from "./scene/serices/web-socket.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    Material2Module
   ],
   providers: [WebSocketService],
   bootstrap: [AppComponent]
