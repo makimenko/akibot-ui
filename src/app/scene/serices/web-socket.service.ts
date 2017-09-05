8b6e92a4d7b5b6be520070411f0de19e4b33b484
makimenko <mihails.akimenko@gmail.com>
2017-09-05 21:57:58 +0300

    connection failoveer

--- src/app/scene/serices/web-socket.service.ts
+++ src/app/scene/serices/web-socket.service.ts
@@ -12,32 +12,54 @@ export class WebSocketService {
     public events: EventEmitter;
 
     // TODO: Mage connectionString configurable: raspberrypi/localhost
-    private connectionString: string = "ws://localhost:3000";
+    private connectionList: string[] = [
+        "ws://localhost:3000",
+        "ws://raspberrypi:3000"
+    ];
+    private connected: boolean = false;
+    private connectIndex: number = -1;
 
     constructor() {
         this.logger.info("constructor");
 
         this.events = new EventEmitter();
 
-        this.logger.debug("Connecting to: " + this.connectionString);
-        this.ws = new WebSocket(this.connectionString);
-
         this.onOpen = this.onOpen.bind(this);
         this.onMessage = this.onMessage.bind(this);
         this.onError = this.onError.bind(this);
 
-        this.ws.onopen = this.onOpen;
-        this.ws.onmessage = this.onMessage;
-        this.ws.onerror = this.onError;
+        this.connectNext();
+    }
+
+    private connectNext() {
+        this.connectIndex++;
+        if (this.connectionList.length > this.connectIndex) {
+            var conn: string = this.connectionList[this.connectIndex];
+            this.logger.debug("Connecting to: " + conn);
+            this.ws = new WebSocket(conn);
+
+            this.ws.onopen = this.onOpen;
+            this.ws.onmessage = this.onMessage;
+            this.ws.onerror = this.onError;
+        } else {
+            throw new Error("Failed to connect");
+        }
     }
 
     public onError(event: Event): any {
-        this.logger.debug("onError");
-        this.events.emit("onerror", event);
+        if (!this.connected) {
+            this.logger.warn("Trying to reconnect to alternative server");
+            this.connectNext();
+        } else {
+            this.logger.error("onError: " + JSON.stringify(event));
+            this.events.emit("onerror", event);
+        }
+
     }
 
     public onOpen(event: Event): any {
         this.logger.debug("onOpen");
+        this.connected = true;
         this.events.emit("onopen", event);
     }
 
